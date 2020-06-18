4b5f3cad24eb2e0668730c0f075708acc7c1f696
Fransiskusandika <dikakoko@icloud.com>
2020-06-18 17:37:43 +0700

    selenium parametirez

---
+++ src/test/java/object/Alert.java
@@ -0,0 +1,88 @@
+package object;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+public class Alert {
+	WebDriver driver;
+	
+	public Alert(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void byId(String selector, String alertText) {
+		WebElement element = driver.findElement(By.id(selector));
+		waitForVisible(driver, element);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element);
+		Assert.assertEquals(element.getText().toLowerCase().replace(" ", ""), alertText.toLowerCase().replace(" ", ""));
+		actions.build().perform();
+		System.out.println(element.getText());
+	}
+	
+	public void byXpath(String selector, String alertText) {
+		WebElement element = driver.findElement(By.xpath(selector));
+		waitForVisible(driver, element);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element);
+		Assert.assertEquals(element.getText().toLowerCase().replace(" ", ""), alertText.toLowerCase().replace(" ", ""));
+		actions.build().perform();
+		System.out.println(element.getText());
+	}
+	
+	public void byCss(String selector, String alertText) {
+		WebElement element = driver.findElement(By.cssSelector(selector));
+		waitForVisible(driver, element);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element);
+		Assert.assertEquals(element.getText().toLowerCase().replace(" ", ""), alertText.toLowerCase().replace(" ", ""));
+		actions.build().perform();
+		System.out.println(element.getText());
+	}
+	
+	public void bytagName(String selector, String alertText) {
+		WebElement element = driver.findElement(By.tagName(selector));
+		waitForVisible(driver, element);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element);
+		Assert.assertEquals(element.getText().toLowerCase().replace(" ", ""), alertText.toLowerCase().replace(" ", ""));
+		actions.build().perform();
+		System.out.println(element.getText());
+	}
+	
+	public void byLinkText(String selector, String alertText) {
+		WebElement element = driver.findElement(By.linkText(selector));
+		waitForVisible(driver, element);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element);
+		Assert.assertEquals(element.getText().toLowerCase().replace(" ", ""), alertText.toLowerCase().replace(" ", ""));
+		actions.build().perform();
+		System.out.println(element.getText());
+	}
+	
+	public void byName(String selector, String alertText) {
+		WebElement element = driver.findElement(By.name(selector));
+		waitForVisible(driver, element);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element);
+		Assert.assertEquals(element.getText().toLowerCase().replace(" ", ""), alertText.toLowerCase().replace(" ", ""));
+		actions.build().perform();
+		System.out.println(element.getText());
+	}
+	
+	private void waitForVisible(WebDriver driver, WebElement element) {
+		try {
+			Thread.sleep(5000);
+			System.out.println("Waiting for element visibility");
+			WebDriverWait wait = new WebDriverWait(driver, 5);
+			wait.until(ExpectedConditions.visibilityOf(element));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ src/test/java/object/Input.java
@@ -0,0 +1,93 @@
+package object;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Input {
+	WebDriver driver;
+	
+	public Input(WebDriver driver) {
+		this.driver = driver;
+	}
+	
+	public void byId(String selector, String value) {
+		WebElement element = driver.findElement(By.id(selector));
+		waitForVisible(driver, element);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element);
+		actions.click();
+		actions.sendKeys(value);
+		actions.build().perform();
+		System.out.println("Input Value: " + value);
+	}
+	
+	public void byXpath(String selector, String value) {
+		WebElement element = driver.findElement(By.xpath(selector));
+		waitForVisible(driver, element);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element);
+		actions.click();
+		actions.sendKeys(value);
+		actions.build().perform();
+		System.out.println("Input Value: " + value);
+	}
+	
+	public void byClass(String selector, String value) {
+		WebElement element = driver.findElement(By.className(selector));
+		waitForVisible(driver, element);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element);
+		actions.click();
+		actions.sendKeys(value);
+		actions.build().perform();
+		System.out.println("Input Value: " + value);
+	}
+	
+	public void byTag(String selector, String value) {
+		WebElement element = driver.findElement(By.tagName(selector));
+		waitForVisible(driver, element);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element);
+		actions.click();
+		actions.sendKeys(value);
+		actions.build().perform();
+		System.out.println("Input Value: " + value);
+	}
+	
+	public void byCss(String selector, String value) {
+		WebElement element = driver.findElement(By.cssSelector(selector));
+		waitForVisible(driver, element);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element);
+		actions.click();
+		actions.sendKeys(value);
+		actions.build().perform();
+		System.out.println("Input Value: " + value);
+	}
+	
+	public void byName(String selector, String value) {
+		WebElement element = driver.findElement(By.name(selector));
+		waitForVisible(driver, element);
+		Actions actions = new Actions(driver);
+		actions.moveToElement(element);
+		actions.click();
+		actions.sendKeys(value);
+		actions.build().perform();
+		System.out.println("Input Value: " + value);
+	}
+	
+	private void waitForVisible(WebDriver driver, WebElement element) {
+		try {
+			Thread.sleep(5000);
+			System.out.println("Waiting for element visibility");
+			WebDriverWait wait = new WebDriverWait(driver, 5);
+			wait.until(ExpectedConditions.visibilityOf(element));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
