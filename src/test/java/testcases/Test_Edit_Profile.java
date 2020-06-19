aed86d0d4712c94da00eabc8e13666764b9eff15
Fransiskusandika <dikakoko@icloud.com>
2020-06-19 19:28:21 +0700

    dd

--- src/test/java/testcases/Test_Edit_Profile.java
+++ src/test/java/testcases/Test_Edit_Profile.java
@@ -1,5 +1,12 @@
 package testcases;
 
-public class Test_Edit_Profile {
+import config.Setup;
 
+public class Test_Edit_Profile extends Setup {
+	
+	public String path = "../pwa/src/main/java/data/data_test.xlsx";
+	
+	public void edit_profile_upload_foto() {
+		
+	}
 }
